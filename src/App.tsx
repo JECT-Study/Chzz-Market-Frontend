f58a19659d1d717b2e9377d77c9a03a1d78cfc87
aquaman122 <swj981128@gmail.com>
2024-07-18 19:22:02 +0900

    chore: router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routing';
 
 function App() {
-  return <div>app</div>;
+  return <RouterProvider router={router} />;
 }
+
 export default App;
---
+++ src/constant/route.ts
@@ -0,0 +1,5 @@
+export const ROUTERS = {
+  MAIN: '/',
+} as const;
+
+export default ROUTERS;
