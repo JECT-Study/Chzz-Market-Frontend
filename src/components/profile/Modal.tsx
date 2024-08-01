931a59b37055fbb3ca9ad98f38a1ac297f38bf44
aquaman122 <swj981128@gmail.com>
2024-08-01 12:41:12 +0900

    feat: 프로필 수정페이지 마크업

--- src/components/profile/Modal.tsx
+++ src/components/profile/Modal.tsx
@@ -12,12 +12,13 @@ const Modal = ({ isOpen, onClose, children }: ModalProps) => {
   return ReactDOM.createPortal(
     <div className="fixed inset-0 bg-black bg-opacity-50">
       <div className="h-full flex justify-center items-end">
-        <div className="w-[46rem] min-w-[23rem] h-2/4 bg-white rounded-lg shadow-lg">
-          <form className="flex justify-end p-2 pr-4">
+        <div className="w-[46rem] min-w-[23rem] h-3/5 bg-white rounded-t-lg shadow-lg p-4 flex flex-col justify-between">
+          <div className="flex justify-between">
+            <h2 className="text-lg font-bold">지역 선택</h2>
             <button className="text-2xl" onClick={onClose}>
               X
             </button>
-          </form>
+          </div>
           {children}
         </div>
       </div>
--- src/pages/ProfileEditPage.tsx
+++ src/pages/ProfileEditPage.tsx
@@ -1,10 +1,9 @@
 import { useState } from 'react';
-import Footer from '@/components/Footer';
 import Layout from '@/components/Layout';
 import Button from '@/components/common/Button';
 import ProfileInput from '@/components/profile/ProfileInput';
-import SelectRegion from '@/components/profile/SelectRegion';
 import Header from '@/components/Header';
+import SelectCountry from '@/components/profile/SelectCountry';
 
 const ProfileEditPage = () => {
   const [profileName, setProfileName] = useState<string>('최대열다섯글자');
@@ -16,19 +15,14 @@ const ProfileEditPage = () => {
   const [link, setLink] = useState('');
 
   const onCloseBottomSheet = () => {
-    setActiveButtonSheet(false);
+    setActiveButtonSheet(!activeButtonSheet);
   };
 
   return (
     <Layout header={<Header>프로필 수정</Header>}>
-      <div className="flex flex-col items-center px-4 py-6 space-y-4">
-        <div className="w-20 h-20 bg-gray-300 rounded-full" />
-        <h2 className="text-lg font-bold">프로필 정보</h2>
-        <ProfileInput
-          title="프로필 이름"
-          value={profileName}
-          onChange={() => {}}
-        />
+      <div className="flex flex-col px-4 py-6 space-y-4">
+        <h2 className="text-lg font-bold pb-4">프로필 정보</h2>
+        <ProfileInput title="닉네임" value={profileName} onChange={() => {}} />
         <div className="w-full">
           <p className="text-gray-600">자기소개</p>
           <textarea className="w-full py-2 h-[60px] border-b" rows={2}>
@@ -39,12 +33,12 @@ const ProfileEditPage = () => {
           <p className="text-gray-600">지역</p>
           <div
             className="w-full py-2 h-[40px] border-b hover:cursor-pointer"
-            onClick={() => setActiveButtonSheet(true)}
+            onClick={() => setActiveButtonSheet(!activeButtonSheet)}
           >
             {profileRegion}
           </div>
         </div>
-        <SelectRegion onClose={onCloseBottomSheet} />
+        {activeButtonSheet && <SelectCountry onClose={onCloseBottomSheet} />}
         <ProfileInput
           title="링크"
           value={link}
