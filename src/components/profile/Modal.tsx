70a8ccdfff96678d6415949f4e9e18df3f7e7f16
aquaman122 <swj981128@gmail.com>
2024-08-06 22:21:36 +0900

    feat: 은행 선택 모달 생성

--- src/components/profile/Modal.tsx
+++ src/components/profile/Modal.tsx
@@ -12,13 +12,7 @@ const Modal = ({ isOpen, onClose, children }: ModalProps) => {
   return ReactDOM.createPortal(
     <div className="fixed inset-0 bg-black bg-opacity-50">
       <div className="h-full flex justify-center items-end">
-        <div className="w-[46rem] min-w-[23rem] h-3/5 bg-white rounded-t-lg shadow-lg p-4 flex flex-col justify-between">
-          <div className="flex justify-between">
-            <h2 className="text-lg font-bold">지역 선택</h2>
-            <button className="text-2xl" onClick={onClose}>
-              X
-            </button>
-          </div>
+        <div className="w-[46rem] min-w-[23rem] h-4/5 bg-white rounded-t-lg shadow-lg p-4 flex flex-col justify-between">
           {children}
         </div>
       </div>
---
+++ src/components/profile/SelectBank.tsx
@@ -0,0 +1,56 @@
+import Button from '../common/Button';
+import XButton from '../common/XButton';
+import Modal from './Modal';
+
+const banks = [
+  { id: 1, name: 'NH농협', img: '/bank_NH.svg' },
+  { id: 2, name: 'KB국민', img: '/bank_KB.svg' },
+  { id: 3, name: '카카오뱅크', img: '/bank_kakao.svg' },
+  { id: 4, name: '신한', img: '/bank_shinhan.svg' },
+  { id: 5, name: '우리', img: '/bank_uri.svg' },
+  { id: 6, name: 'IBK기업', img: '/bank_IBK.svg' },
+  { id: 7, name: '하나', img: '/bank_hana.svg' },
+  { id: 8, name: '새마을', img: '/bank_saemaul.svg' },
+  { id: 9, name: '시티', img: '/bank_CITY.svg' },
+  { id: 10, name: 'K뱅크', img: '/bank_Kbank.svg' },
+];
+
+interface Props {
+  onClose: () => void;
+}
+
+const SelectBank = ({ onClose }: Props) => {
+  return (
+    <Modal isOpen onClose={onClose}>
+      <div className="relative flex justify-center mb-4">
+        <h2 className="text-xl font-bold">은행</h2>
+        <button
+          className="absolute right-2 text-2xl"
+          onClick={onClose}
+          aria-label="닫기"
+        >
+          <XButton />
+        </button>
+      </div>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-3 gap-2">
+          {banks.map((item) => (
+            <Button
+              key={item.id}
+              className="flex flex-col items-center justify-center w-24 h-24 border border-gary-300 rounded-lg shadow-sm hover:bg-gray-100 transition duration-300"
+              color="gray3"
+            >
+              <img src={item.img} alt={item.name} className="w-10 h-10 mb-2" />
+              <p className="text-black text-sm font-medium">{item.name}</p>
+            </Button>
+          ))}
+        </div>
+      </div>
+      <Button size="medium" className="rounded-lg" color="cheeseYellow">
+        지역 선택 완료
+      </Button>
+    </Modal>
+  );
+};
+
+export default SelectBank;
