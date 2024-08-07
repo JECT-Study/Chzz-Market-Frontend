c5f8ee942815f9e7fb727c764cb58c9da175257a
aquaman122 <swj981128@gmail.com>
2024-08-07 19:56:00 +0900

    chore: 절대 경로 models 추가

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
       '@/pages': resolve(__dirname, 'src/pages'),
       '@/api': resolve(__dirname, 'src/api'),
       '@/utils': resolve(__dirname, 'src/utils'),
+      '@/models': resolve(__dirname, 'src/models'),
     },
   },
   test: {
