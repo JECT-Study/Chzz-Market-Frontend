5ada65ed09ea1ce4451136f6f16fed8d7ba72148
aquaman122 <swj981128@gmail.com>
2024-08-09 18:41:40 +0900

    feat: login Oauth 기능 구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,35 @@
 import Button from '@/components/common/Button';
 import Layout from '@/components/Layout';
-import { RiKakaoTalkFill } from 'react-icons/ri';
+import { Window } from '@/models/kakao';
 import { SiNaver } from 'react-icons/si';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const navigate = useNavigate();
+
+  const handleKakaoLogin = () => {
+    if ((window as Window).Kakao) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (window as any).Kakao.Auth.authorize({
+        redirectUri: 'http://localhost:5173/login/kakao/code',
+      });
+    }
+  };
+
   return (
     <Layout>
       <Layout.Header handleBack={() => navigate('/')}>로그인</Layout.Header>
       <Layout.Main>
         <div className="flex flex-col items-center h-full">
           {/* <img /> */}
-          <div className="w-full">
-            <img src="/kakao_login_medium_narrow.png" alt="kakaoButton" />
+          <div className="w-full h-[52px]">
+            <button className="w-full h-[52px]" onClick={handleKakaoLogin}>
+              <img
+                src="/kakao_login_large_wide.png"
+                alt="kakaoButton"
+                className="w-full h-full object-contain "
+              />
+            </button>
             <Button
               className="relative flex items-center justify-center w-full border-none rounded-md bg-mainCheeseYellow"
               size="large"
