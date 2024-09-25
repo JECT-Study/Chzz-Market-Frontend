3b4e27a3dae0709ebeeab886858cd798b1e43d44
aquaman122 <swj981128@gmail.com>
2024-09-25 20:19:33 +0900

    chore: 주석 삭제 및 코드수정

--- src/components/user/UserOrder.tsx
+++ src/components/user/UserOrder.tsx
@@ -12,8 +12,8 @@ const UserOrder = () => {
     <div className="pb-10">
       {/* 참여한 정식 경매 내역 */}
       <div>
-        <h2 className="text-xl font-bold mb-4">참여한 정식 경매 내역</h2>
-        <div className="flex justify-between space-x-3">
+        <h2 className="text-heading1 mb-4">참여한 정식 경매 내역</h2>
+        <div className="flex justify-between gap-3">
           {/* 진행중인 경매 */}
           <div
             className="flex flex-col items-center p-4 border border-gray2 rounded-lg w-1/3 cursor-pointer"
@@ -62,8 +62,8 @@ const UserOrder = () => {
 
       {/* 내가 등록한 경매 내역 */}
       <div className="mt-8">
-        <h2 className="text-xl font-bold mb-4">내가 등록한 경매 내역</h2>
-        <div className="flex justify-between space-x-3">
+        <h2 className="text-heading1 mb-4">내가 등록한 경매 내역</h2>
+        <div className="flex justify-between gap-3">
           {/* 정식 경매 */}
           <div
             className="flex flex-col items-center p-4 border border-gray2 rounded-lg w-1/2 cursor-pointer"
