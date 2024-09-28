46822af3c2d7dfc79db8041ad2302c009100ae22
JunSeok <ppu1234@naver.com>
2024-09-28 22:57:15 +0900

    type: register type 추가

--- src/@types/Register.d.ts
+++ src/@types/Register.d.ts
@@ -1,3 +1,9 @@
 declare module 'Register' {
-  export interface Register {}
+  export interface Register {
+    productName: string;
+    description: string;
+    minPrice: number;
+    auctionRegisterType: string;
+    category: string;
+  }
 }
