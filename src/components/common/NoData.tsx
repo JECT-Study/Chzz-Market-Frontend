15cac93348ecf4d818f0675839c500d4d1b98613
JunSeok <ppu1234@naver.com>
2024-09-28 22:58:37 +0900

    feat: no data 추가

---
+++ src/components/common/NoData.tsx
@@ -0,0 +1,9 @@
+const NoData = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-full text-heading1 text-cheeseYellow">
+      알림 없음.
+    </div>
+  );
+};
+
+export default NoData;
