0729a8cf84e1d3a0c5657a294992c6744dba043d
CLOUDoort <ppu1234@naver.com>
2024-09-29 04:48:33 +0000

    fix: 기타

--- src/components/register/quries.ts
+++ src/components/register/quries.ts
@@ -3,7 +3,6 @@ import { UseMutateFunction, useMutation } from '@tanstack/react-query';
 import { API_END_POINT } from '@/constants/api';
 import { httpClient } from '@/api/axios';
 import { useNavigate } from 'react-router-dom';
-import { toast } from 'sonner';
 
 export const usePostRegister = (): {
   mutate: UseMutateFunction<unknown, Error, FormData, unknown>;
