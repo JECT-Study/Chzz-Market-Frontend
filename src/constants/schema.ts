ba5c108e970c4eb01beba1700b1622e43fec6ec6
JunSeok <ppu1234@naver.com>
2024-09-30 12:34:37 +0900

    fix: register validation

--- src/constants/schema.ts
+++ src/constants/schema.ts
@@ -1,10 +1,7 @@
 import { z } from 'zod';
 
 export const SignupFormSchema = z.object({
-  nickname: z
-    .string()
-    .min(1, '닉네임은 최소 1자 이상 입력해 주세요.')
-    .max(15, '닉네임은 최대 15자 이하로 입력해 주세요.'),
+  nickname: z.string().min(1, '닉네임은 최소 1자 이상 입력해 주세요.').max(15, '닉네임은 최대 15자 이하로 입력해 주세요.'),
   bankName: z.string(),
   accountNumber: z
     .string()
@@ -16,36 +13,30 @@ export const SignupFormSchema = z.object({
   link: z
     .string()
     .optional()
-    .refine(
-      (value) => !value || /^(https?:\/\/)/.test(value),
-      '링크는 http:// 또는 https://로 시작해야 합니다.',
-    ),
+    .refine((value) => !value || /^(https?:\/\/)/.test(value), '링크는 http:// 또는 https://로 시작해야 합니다.'),
 });
 
 export const RegisterSchema = z.object({
-  productName: z
-    .string()
-    .min(2, '제목은 최소 2자 이상 입력해 주세요.')
-    .max(30, '제목은 최대 30자 이하로 입력해 주세요.'),
-  images: z
-    .array(z.string())
-    .min(1, '사진은 최소 1장 이상 등록해 주세요.')
-    .max(5, '사진은 최대 5장 이하로 등록해 주세요.'),
+  productName: z.string().min(2, '제목은 최소 2자 이상 입력해 주세요.').max(30, '제목은 최대 30자 이하로 입력해 주세요.'),
+  images: z.array(z.string()).min(1, '사진은 최소 1장 이상 등록해 주세요.').max(5, '사진은 최대 5장 이하로 등록해 주세요.'),
   category: z.string().min(1, '카테고리를 선택해 주세요.'),
-  minPrice: z.string().refine(
-    (value) => {
-      const num = Number(value.replace(/[^\d]/g, '')); // 숫자만 추출
-      return !Number.isNaN(num) && num >= 1000; // 숫자가 1000 이상이어야만 true 반환
-    },
-    {
-      message: '최소 1000원 이상 입력해 주세요.',
-    },
-  ),
-  description: z
-    .string()
-    .min(5, '상품 설명은 최소 5자 이상 입력해 주세요.')
-    .max(1000, '상품 설명은 최대 1000자 이하로 입력해 주세요.')
-    .or(z.literal('')),
+  minPrice: z.string().superRefine((value, ctx) => {
+    const num = Number(value.replace(/[^\d]/g, ''));
+    if (Number.isNaN(num) || num < 1000) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: '최소 1000원 이상 입력해 주세요.',
+      });
+    }
+
+    if (num % 1000 !== 0) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: '1000원 단위로 입력해 주세요.',
+      });
+    }
+  }),
+  description: z.string().min(5, '상품 설명은 최소 5자 이상 입력해 주세요.').max(1000, '상품 설명은 최대 1000자 이하로 입력해 주세요.').or(z.literal('')),
 });
 
 export const AddressBookSchema = z.object({
