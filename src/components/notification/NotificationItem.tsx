444b774094cc8aab0ee393be4757b2e900dba5dc
JunSeok <ppu1234@naver.com>
2024-10-01 19:48:28 +0900

    feat: 기본 이미지 설정

--- src/components/notification/NotificationItem.tsx
+++ src/components/notification/NotificationItem.tsx
@@ -3,6 +3,7 @@ import type { INotification } from 'Notification';
 import XButtonIcon from '@/assets/icons/x_button.svg';
 import { getTimeAgo } from '@/utils/getTimeAgo';
 import { useNavigate } from 'react-router-dom';
+import { addDefaultImg } from '@/utils/addDefaultImg';
 
 const NotificationItem = ({
   item,
@@ -35,7 +36,7 @@ const NotificationItem = ({
             {time}
           </div>
         </figcaption>
-        <img className='object-cover rounded size-24' src={cdnPath} alt={`알림 이미지_${item.id}`} />
+        <img className='object-cover rounded size-24' src={cdnPath} onError={addDefaultImg} alt={`알림 이미지_${item.id}`} />
       </figure>
       <button aria-label={`알림 삭제 버튼_${item.id}`} onClick={() => handleDelete(item.id)}>
         <img className='p-2 rounded size-8 hover:bg-black/30 active:bg-black/60' src={XButtonIcon} alt='알림 삭제 아이콘' />
