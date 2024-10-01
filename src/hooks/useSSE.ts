636d6ec28a334c1a7b1af6c5796cb1b6c904dfeb
JunSeok <ppu1234@naver.com>
2024-10-01 09:56:34 +0900

    fix: SSE 45000ms error 해결

--- src/hooks/useSSE.ts
+++ src/hooks/useSSE.ts
@@ -2,41 +2,45 @@ import { EventSourcePolyfill, NativeEventSource } from 'event-source-polyfill';
 import { useEffect, useRef, useState } from 'react';
 
 import { getToken } from '@/utils/tokenUtils';
+import { useSelector } from 'react-redux';
+import { isLoggedIn } from '@/store/authSlice';
 
 export const useSSE = <T>(url: string) => {
   const [state, setState] = useState<T[]>([]);
   const EventSource = EventSourcePolyfill || NativeEventSource;
   const eventSource = useRef<null | EventSource>(null);
+  const accessToken = getToken();
+  const isLogin = useSelector(isLoggedIn);
 
   useEffect(() => {
     const fetchSSE = () => {
-      const accessToken = getToken();
-      if (accessToken) {
-        eventSource.current = new EventSource(`${import.meta.env.VITE_API_URL}${url}`, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-          withCredentials: true,
-        });
+      eventSource.current = new EventSource(`${import.meta.env.VITE_API_URL}${url}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        heartbeatTimeout: 600000,
+        withCredentials: true,
+      });
 
-        eventSource.current.onopen = () => {};
+      eventSource.current.onopen = () => {};
 
-        eventSource.current.onerror = () => {
-          eventSource.current?.close();
-          setTimeout(fetchSSE, 3000);
-        };
+      eventSource.current.onerror = () => {
+        eventSource.current?.close();
+        setTimeout(fetchSSE, 3000);
+      };
 
-        eventSource.current.addEventListener('init', () => {});
+      eventSource.current.addEventListener('init', () => {});
 
-        eventSource.current.addEventListener('notification', (e) => {
-          const data = JSON.parse(e.data);
-          setState((prev) => [...prev, data]);
-        });
-      }
+      eventSource.current.addEventListener('notification', (e) => {
+        const data = JSON.parse(e.data);
+        setState((prev) => [...prev, data]);
+      });
     };
-    fetchSSE();
+
+    if (isLogin) fetchSSE();
+
     return () => eventSource.current?.close();
-  }, [url, EventSource]);
+  }, [url, isLogin]);
 
   return { state, setState };
 };
