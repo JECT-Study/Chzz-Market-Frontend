9ccc76c6d4cf1abc5d1104f9f40baee2345dfe35
JunSeok <ppu1234@naver.com>
2024-10-01 09:57:37 +0900

    fix: 로그인 후 홈페이지 리다이렉트하는 경우 toast 제한

--- src/components/common/route/PublicRoute.tsx
+++ src/components/common/route/PublicRoute.tsx
@@ -1,14 +1,15 @@
 import { isLoggedIn } from '@/store/authSlice';
 import { ReactNode } from 'react';
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { toast } from 'sonner';
 
 const PublicRoute = ({ children }: { children: ReactNode }) => {
   const isLogin = useSelector(isLoggedIn);
+  const { search } = useLocation();
 
   if (isLogin) {
-    toast.warning('이미 로그인된 사용자입니다.');
+    if (search === '') toast.warning('이미 로그인된 사용자입니다.');
     return <Navigate to='/' replace />;
   }
 
