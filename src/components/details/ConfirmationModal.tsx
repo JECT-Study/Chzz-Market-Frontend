cae76865d5a8ceb73a2541c1096f65747b186d58
luminox93 <memoji1236@gmail.com>
2024-10-02 17:38:52 +0900

    feat: props type 지정

--- src/components/details/ConfirmationModal.tsx
+++ src/components/details/ConfirmationModal.tsx
@@ -1,24 +1,27 @@
-// ConfirmationModal.tsx
-import React from 'react';
+interface ConfirmationModalProps {
+  message: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+}
 
-const ConfirmationModal = ({ message, onConfirm, onCancel }) => {
+const ConfirmationModal = ({ message, onConfirm, onCancel }: ConfirmationModalProps) => {
   return (
-    <div className='fixed inset-0 flex items-center justify-center z-50'>
+    <div className="fixed inset-0 flex items-center justify-center z-50">
       <div
-        className='absolute inset-0 bg-black opacity-50'
+        className="absolute inset-0 bg-black opacity-50"
         onClick={onCancel}
-      ></div>
-      <div className='bg-white p-6 rounded-md shadow-md z-10'>
-        <p className='mb-4'>{message}</p>
-        <div className='flex justify-end'>
+      />
+      <div className="bg-white p-6 rounded-md shadow-md z-10">
+        <p className="mb-4">{message}</p>
+        <div className="flex justify-end">
           <button
-            className='px-4 py-2 mr-2 text-gray-700 bg-gray-200 rounded'
+            className="px-4 py-2 mr-2 text-gray-700 bg-gray-200 rounded"
             onClick={onCancel}
           >
             취소
           </button>
           <button
-            className='px-4 py-2 text-white bg-red-600 rounded'
+            className="px-4 py-2 text-white bg-red-600 rounded"
             onClick={onConfirm}
           >
             삭제
