110c2b0c9945d17213dc3056320f97d4860f677e
luminox93 <memoji1236@gmail.com>
2024-10-02 15:09:38 +0900

    feat: 삭제 확인 안내 modal 생성

---
+++ src/components/details/SuccessModal.tsx
@@ -0,0 +1,26 @@
+// SuccessModal.tsx
+import React from 'react';
+
+const SuccessModal = ({ message, onClose }) => {
+  return (
+    <div className='fixed inset-0 flex items-center justify-center z-50'>
+      <div
+        className='absolute inset-0 bg-black opacity-50'
+        onClick={onClose}
+      ></div>
+      <div className='bg-white p-6 rounded-md shadow-md z-10'>
+        <p className='mb-4'>{message}</p>
+        <div className='flex justify-end'>
+          <button
+            className='px-4 py-2 text-white bg-blue-600 rounded'
+            onClick={onClose}
+          >
+            닫기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessModal;
