05adddb06bc0e7fd00a0ae720cf264c43d3aca9b
JunSeok <ppu1234@naver.com>
2024-10-04 16:42:37 +0900

    feat: Carousel 적용

--- src/components/home/HomeItemList.tsx
+++ src/components/home/HomeItemList.tsx
@@ -1,16 +1,10 @@
 import { ReactNode } from 'react';
 
-const HomeItemList = ({
-  children,
-  name,
-}: {
-  children: ReactNode;
-  name: string;
-}) => {
+const HomeItemList = ({ children, name }: { children: ReactNode; name: string }) => {
   return (
-    <section className="flex flex-col w-full gap-4">
-      <label className="text-lg font-semibold">{name}</label>
-      <div className="flex gap-4 overflow-x-scroll">{children}</div>
+    <section className='flex flex-col w-full gap-4'>
+      <label className='text-lg font-semibold'>{name}</label>
+      <div className='flex gap-4'>{children}</div>
     </section>
   );
 };
