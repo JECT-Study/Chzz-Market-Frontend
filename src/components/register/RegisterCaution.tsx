fda835d9a59db5c6aa846bb9e7af29e4a10348db
JunSeok <ppu1234@naver.com>
2024-10-04 16:42:52 +0900

    fix: 경매 등록 수정

--- src/components/register/RegisterCaution.tsx
+++ src/components/register/RegisterCaution.tsx
@@ -1,8 +1,4 @@
-import {
-  ENROLLMENT_CAUTION,
-  PRE_ENROLLMENT_CAUTION,
-} from '@/constants/caution';
-
+import { PRE_REGISTER_CAUTION, REGISTER_CAUTION } from '@/constants/caution';
 import CautionCheck from '../common/CautionCheck';
 
 interface CautionProps {
@@ -12,34 +8,29 @@ interface CautionProps {
 }
 
 const RegisterCaution = ({ kind, check, handleCheck }: CautionProps) => {
+  console.log(kind);
   return (
-    <section className="flex flex-col pt-5 gap-[3rem]">
-      <h3 className="text-heading2">
-        {kind === 'enroll'
-          ? ENROLLMENT_CAUTION.HEADING
-          : PRE_ENROLLMENT_CAUTION.HEADING}
-      </h3>
-      <div className="space-y-5">
-        {kind === 'enroll' ? (
+    <section className='flex flex-col pt-5 gap-[3rem]'>
+      <h3 className='text-heading2'>{kind === 'REGISTER' ? REGISTER_CAUTION.HEADING : PRE_REGISTER_CAUTION.HEADING}</h3>
+      <div className='space-y-5'>
+        {kind === 'REGISTER' ? (
           <>
-            <h4 className="text-body1Bold">{ENROLLMENT_CAUTION.TITLE}</h4>
-            {Object.entries(ENROLLMENT_CAUTION.CONTENT).map(([key, value]) => (
-              <div key={key} className="space-y-1 text-body2 text-gray2">
+            <h4 className='text-body1Bold'>{REGISTER_CAUTION.TITLE}</h4>
+            {Object.entries(REGISTER_CAUTION.CONTENT).map(([key, value]) => (
+              <div key={key} className='space-y-1 text-body2 text-gray2'>
                 <h5>{value.TITLE}</h5>
-                <p className="pl-3">{value.DESCRIPTION}</p>
+                <p className='pl-3'>{value.DESCRIPTION}</p>
               </div>
             ))}
           </>
         ) : (
-          <div className="space-y-5">
-            {Object.entries(PRE_ENROLLMENT_CAUTION.CONTENT).map(
-              ([key, value]) => (
-                <div className="space-y-4" key={key}>
-                  <h4 className="text-body1Bold">{value.TITLE}</h4>
-                  <p className="text-body2 text-gray2">{value.DESCRIPTION}</p>
-                </div>
-              ),
-            )}
+          <div className='space-y-5'>
+            {Object.entries(PRE_REGISTER_CAUTION.CONTENT).map(([key, value]) => (
+              <div className='space-y-4' key={key}>
+                <h4 className='text-body1Bold'>{value.TITLE}</h4>
+                <p className='text-body2 text-gray2'>{value.DESCRIPTION}</p>
+              </div>
+            ))}
           </div>
         )}
         <CautionCheck check={check} handleCheck={handleCheck} />
