57d121446da9a3839decc53de069f358b9206a48
JunSeok <ppu1234@naver.com>
2024-10-04 23:43:09 +0900

    fix: error message type 수정

--- src/constants/emptyMessage.ts
+++ src/constants/emptyMessage.ts
@@ -3,7 +3,7 @@ export const EMPTY_MESSAGE: { [key: string]: string } = Object.freeze({
   heart: '관심 있는 사전 경매가 없어요.',
   imminent: '종료 임박 경매가 없어요.',
   best: '베스트 경매가 없어요.',
-  preRegisterAuction: '사전 등록 경매가 없어요.',
+  preAuction: '사전 등록 경매가 없어요.',
   userRegisterAuction: '내가 등록한 경매가 없어요.',
   userPreRegisterAuction: '내가 등록한 사전 경매가 없어요.',
 });
