f13a45e0e837d3f2c31e8dac3aea8aa8a23ab848
luminox93 <memoji1236@gmail.com>
2024-10-07 21:01:12 +0900

    fix: 구매자가 가격 수정횟수가 다 될때까지 경매 가격 입찰 가능하도록 수정

--- src/components/details/BuyersFooter.tsx
+++ src/components/details/BuyersFooter.tsx
@@ -14,7 +14,14 @@ interface BuyersFooterProps {
   remainingBidCount?: number;
 }
 
-const BuyersFooter = ({ auctionId, isSeller, status, likeCount = 0, isParticipated, remainingBidCount }: BuyersFooterProps) => {
+const BuyersFooter = ({
+  auctionId,
+  isSeller,
+  status,
+  likeCount = 0,
+  isParticipated,
+  remainingBidCount,
+}: BuyersFooterProps) => {
   const navigate = useNavigate();
   const { mutate } = useLikeAuctionItem();
 
@@ -46,7 +53,12 @@ const BuyersFooter = ({ auctionId, isSeller, status, likeCount = 0, isParticipat
       <div className='flex items-center flex-1 h-full gap-2'>
         <HeartIcon className={`text-xl ${heartColor}`} />
         <span className='text-gray-600'>{`${currentLikeCount}명`}</span>
-        <Button type='button' className='flex-[2] h-full' color='cheeseYellow' onClick={onToggleNotificationHandler}>
+        <Button
+          type='button'
+          className='flex-[2] h-full'
+          color='cheeseYellow'
+          onClick={onToggleNotificationHandler}
+        >
           {isLiked ? '알림 신청 완료' : '오픈 알림 받기'}
         </Button>
       </div>
@@ -56,18 +68,37 @@ const BuyersFooter = ({ auctionId, isSeller, status, likeCount = 0, isParticipat
   if (status === 'PROCEEDING' && !isParticipated) {
     return (
       <div className='flex items-center flex-1 h-full gap-2'>
-        <Button type='button' className='flex-[2] h-full' color='cheeseYellow' onClick={onMoveToBidHandler}>
+        <Button
+          type='button'
+          className='flex-[2] h-full'
+          color='cheeseYellow'
+          onClick={onMoveToBidHandler}
+        >
           경매 참여하기
         </Button>
       </div>
     );
   }
 
-  if (status === 'PROCEEDING' && isParticipated && remainingBidCount && remainingBidCount > 0) {
+  if (
+    status === 'PROCEEDING' &&
+    isParticipated &&
+    remainingBidCount &&
+    remainingBidCount > 0
+  ) {
     return (
       <div className='flex items-center justify-between p-2 rounded-lg'>
-        <button className='px-4 py-2 text-gray-600 border border-gray-400 rounded-lg'>참여 취소</button>
-        <button className='px-4 py-2 text-white transition-colors bg-orange-500 rounded-lg hover:bg-orange-600'>금액 수정({remainingBidCount}회 가능)</button>
+        <button className='px-4 py-2 text-gray-600 border border-gray-400 rounded-lg'>
+          참여 취소
+        </button>
+        <Button
+          type='button'
+          className='flex-[2] h-full'
+          color='cheeseYellow'
+          onClick={onMoveToBidHandler}
+        >
+          금액 수정({remainingBidCount}회 가능)
+        </Button>
       </div>
     );
   }
@@ -75,14 +106,20 @@ const BuyersFooter = ({ auctionId, isSeller, status, likeCount = 0, isParticipat
   if (status === 'PROCEEDING' && isParticipated && remainingBidCount === 0) {
     return (
       <div className='flex items-center justify-between p-2 rounded-lg'>
-        <button className='px-4 py-2 text-gray-600 border border-gray-400 rounded-lg'>참여 취소</button>
-        <button className='px-4 py-2 text-white bg-gray-400 rounded-lg cursor-not-allowed'>금액 수정(소진)</button>
+        <button className='px-4 py-2 text-gray-600 border border-gray-400 rounded-lg'>
+          참여 취소
+        </button>
+        <button className='px-4 py-2 text-white bg-gray-400 rounded-lg cursor-not-allowed'>
+          금액 수정(소진)
+        </button>
       </div>
     );
   }
 
   if (status === 'ENDED') {
-    return <div className='p-2 text-center bg-gray-300 rounded-lg'>종료된 경매</div>;
+    return (
+      <div className='p-2 text-center bg-gray-300 rounded-lg'>종료된 경매</div>
+    );
   }
 
   return null;
