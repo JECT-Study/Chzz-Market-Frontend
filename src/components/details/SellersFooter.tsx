1f64029c3d473ab0a5749be882f86b48796ef2b3
luminox93 <memoji1236@gmail.com>
2024-10-07 20:30:28 +0900

    feat: SellersFooter 에서 사전 경매일 경우 경매 전환하기 버튼을 눌러 경매로 전환하도록 수정

--- src/components/details/SellersFooter.tsx
+++ src/components/details/SellersFooter.tsx
@@ -2,18 +2,26 @@
 import React from 'react';
 import Button from '@/components/common/Button';
 import { AiOutlineHeart } from 'react-icons/ai';
+import { useConvertToAuction } from './queries';
 
 interface SellersFooterProps {
   isSeller: boolean;
   likeCount?: number;
   status?: string;
+  auctionId: number;
 }
 
 const SellersFooter: React.FC<SellersFooterProps> = ({
   isSeller,
   status,
   likeCount = 0,
+  auctionId,
 }) => {
+  const { mutate: convertToAuction } = useConvertToAuction();
+  const onConvertClickHandler = () => {
+    convertToAuction(auctionId);
+  };
+
   if (isSeller && status === 'PROCEEDING') {
     return (
       <div className='bg-gray-300 text-gray-600 p-4 rounded-lg text-center border border-dashed border-blue-200'>
@@ -27,7 +35,12 @@ const SellersFooter: React.FC<SellersFooterProps> = ({
       <div className='flex items-center flex-1 h-full gap-2'>
         <AiOutlineHeart className='text-xl text-gray-500' />
         <span className='text-gray-600'>{`${likeCount}명`}</span>
-        <Button type='button' className='flex-[2] h-full' color='cheeseYellow'>
+        <Button
+          type='button'
+          className='flex-[2] h-full'
+          color='cheeseYellow'
+          onClick={onConvertClickHandler}
+        >
           경매로 전환하기
         </Button>
       </div>
