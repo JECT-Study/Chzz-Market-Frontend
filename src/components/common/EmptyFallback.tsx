fb83092d6466e627730da1f19281fe43b4d0e019
JunSeok <ppu1234@naver.com>
2024-10-07 12:08:59 +0900

    feat:emptyBoundary 적용

--- src/components/common/EmptyFallback.tsx
+++ src/components/common/EmptyFallback.tsx
@@ -1,7 +1,7 @@
 import { EMPTY_MESSAGE } from '@/constants/emptyMessage';
 
 const EmptyFallback = ({ type }: { type: string }) => {
-  return <div className='flex items-center justify-center w-full h-full md:text-heading1 text-heading3 text-mainCheeseYellow'>{EMPTY_MESSAGE[type]}</div>;
+  return <div className='flex items-center justify-center w-full h-full md:text-heading2 text-heading3 text-gray2'>{EMPTY_MESSAGE[type]}</div>;
 };
 
 export default EmptyFallback;
