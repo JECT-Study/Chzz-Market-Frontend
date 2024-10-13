6259b2e861633aaf58c6afd76d5a24a18e260394
luminox93 <memoji1236@gmail.com>
2024-10-14 06:17:06 +0900

    fix: Button 수정 1. Button component로 통일

--- src/components/details/BuyersFooter.tsx
+++ src/components/details/BuyersFooter.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Button from '@/components/common/Button';
-import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
-import { useLikeAuctionItem, useCancelBid } from '@/components/details/queries';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "@/components/common/Button";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { useLikeAuctionItem, useCancelBid } from "@/components/details/queries";
 
 interface BuyersFooterProps {
   auctionId: number;
@@ -47,38 +47,38 @@ const BuyersFooter = ({
 
   const onCancelBidHandler = () => {
     cancelBid(bidId);
-    navigate('/');
+    navigate("/");
   };
 
   const HeartIcon = isLiked ? AiFillHeart : AiOutlineHeart;
-  const heartColor = isLiked ? 'text-red-500' : 'text-gray-500';
+  const heartColor = isLiked ? "text-red-500" : "text-gray-500";
 
   if (isSeller) return null;
 
-  if (status === 'PENDING') {
+  if (status === "PENDING") {
     return (
-      <div className='flex items-center flex-1 h-full gap-2'>
+      <div className="flex items-center flex-1 h-full gap-2">
         <HeartIcon className={`text-xl ${heartColor}`} />
-        <span className='text-gray-600'>{`${currentLikeCount}명`}</span>
+        <span className="text-gray-600">{`${currentLikeCount}명`}</span>
         <Button
-          type='button'
-          className='flex-[2] h-full'
-          color='cheeseYellow'
+          type="button"
+          className="flex-[2] h-full"
+          color="cheeseYellow"
           onClick={onToggleNotificationHandler}
         >
-          {isLiked ? '알림 신청 완료' : '오픈 알림 받기'}
+          {isLiked ? "알림 신청 완료" : "오픈 알림 받기"}
         </Button>
       </div>
     );
   }
 
-  if (status === 'PROCEEDING' && !isParticipated) {
+  if (status === "PROCEEDING" && !isParticipated) {
     return (
-      <div className='flex items-center flex-1 h-full gap-2'>
+      <div className="flex items-center flex-1 h-full gap-2">
         <Button
-          type='button'
-          className='flex-[2] h-full'
-          color='cheeseYellow'
+          type="button"
+          className="flex-[2] h-full"
+          color="cheeseYellow"
           onClick={onMoveToBidHandler}
         >
           경매 참여하기
@@ -88,25 +88,25 @@ const BuyersFooter = ({
   }
 
   if (
-    status === 'PROCEEDING' &&
+    status === "PROCEEDING" &&
     isParticipated &&
     remainingBidCount &&
     remainingBidCount > 0
   ) {
     return (
-      <div className='flex items-center justify-between p-2 rounded-lg'>
+      <div className="flex items-center justify-between p-2 rounded-lg">
         <Button
-          type='button'
-          color='white'
-          className='flex-1 h-full'
-          onClick={onCancelBidHandler} // Call the handler here
+          type="button"
+          color="white"
+          className="flex-1 h-full transition-colors rounded text-button active:bg-black"
+          onClick={onCancelBidHandler}
         >
           참여 취소
         </Button>
         <Button
-          type='button'
-          className='flex-[2] h-full'
-          color='cheeseYellow'
+          type="button"
+          className="flex-[2] h-full"
+          color="cheeseYellow"
           onClick={onMoveToBidHandler}
         >
           금액 수정({remainingBidCount}회 가능)
@@ -115,22 +115,27 @@ const BuyersFooter = ({
     );
   }
 
-  if (status === 'PROCEEDING' && isParticipated && remainingBidCount === 0) {
+  if (status === "PROCEEDING" && isParticipated && remainingBidCount === 0) {
     return (
-      <div className='flex items-center justify-between p-2 rounded-lg'>
-        <button className='px-4 py-2 text-gray-600 border border-gray-400 rounded-lg'>
+      <div className="flex items-center justify-between p-2 rounded-lg">
+        <Button
+          type="button"
+          color="white"
+          className="flex-1 h-full transition-colors rounded text-button active:bg-black"
+          onClick={onCancelBidHandler}
+        >
           참여 취소
-        </button>
-        <Button type='button' className='px-4 py-2 text-gray-600disabled:'>
+        </Button>
+        <Button type="button" className="px-4 py-2 text-gray-600 disabled:">
           금액 수정(소진)
         </Button>
       </div>
     );
   }
 
-  if (status === 'ENDED') {
+  if (status === "ENDED") {
     return (
-      <div className='p-2 text-center bg-gray-300 rounded-lg'>종료된 경매</div>
+      <div className="p-2 text-center bg-gray-300 rounded-lg">종료된 경매</div>
     );
   }
 
