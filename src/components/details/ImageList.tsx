b403ef8d603a0af8aefff2760d0a22c6fc10483d
luminox93 <memoji1236@gmail.com>
2024-10-15 05:17:49 +0900

    feat: ImageList 컴포넌트 생성 1. 케로셀을 랜더링할 ImageList 컴포넌트 생성 2. useImageUrls hook을 사용하여 AuctionDetail/PreAuctionDetail 모두 동일한 방법으로 사용 가능하게 설계 3. key값 중복을 방지하고자 productId{id번호}_{img.key}값으로 설정

---
+++ src/components/details/ImageList.tsx
@@ -0,0 +1,27 @@
+import useImageUrls from '@/hooks/useImageUrls';
+import CustomCarousel from '../common/CustomCarousel';
+import { CarouselItem } from '../ui/carousel'; // CarouselItem을 import합니다.
+import ImageItem from './ImageItem';
+
+interface ImageListProps {
+  images: string[] | { imageId?: number; imageUrl: string }[];
+  productName: string;
+  productId: number;
+}
+
+const ImageList = ({ images, productName, productId }: ImageListProps) => {
+  const imageUrls = useImageUrls(images);
+  const { length } = imageUrls;
+
+  return (
+    <CustomCarousel length={length}>
+      {imageUrls.map((img) => (
+        <CarouselItem key={`productId${productId}_${img.key}`}>
+          <ImageItem url={img.imageUrl} productName={productName} />
+        </CarouselItem>
+      ))}
+    </CustomCarousel>
+  );
+};
+
+export default ImageList;
