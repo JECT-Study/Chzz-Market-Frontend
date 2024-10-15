87d3259444d3a7a3a922415bf2b9b7d7e558f639
luminox93 <memoji1236@gmail.com>
2024-10-15 11:47:19 +0900

    fix: 중복 수정 ImageItem에 CarouselItem이 이미 존재하여 ImageList의 CarouselItem 제거

--- src/components/details/ImageList.tsx
+++ src/components/details/ImageList.tsx
@@ -1,6 +1,5 @@
 import useImageUrls from '@/hooks/useImageUrls';
 import CustomCarousel from '../common/CustomCarousel';
-import { CarouselItem } from '../ui/carousel';
 import ImageItem from './ImageItem';
 
 interface ImageListProps {
@@ -16,9 +15,11 @@ const ImageList = ({ images, productName, productId }: ImageListProps) => {
   return (
     <CustomCarousel length={length}>
       {imageUrls.map((img) => (
-        <CarouselItem key={`productId${productId}_${img.key}`}>
-          <ImageItem url={img.imageUrl} productName={productName} />
-        </CarouselItem>
+        <ImageItem
+          key={`productId${productId}_${img.key}`}
+          url={img.imageUrl}
+          productName={productName}
+        />
       ))}
     </CustomCarousel>
   );
