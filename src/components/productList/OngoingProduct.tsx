4c63b8668e77c4f091793b764da8b11fe6d40042
aquaman122 <swj981128@gmail.com>
2024-10-17 15:28:04 +0900

    chore: 공통 컴포넌트 사용

--- src/components/productList/OngoingProduct.tsx
+++ src/components/productList/OngoingProduct.tsx
@@ -1,32 +1,18 @@
 import Button from '../common/Button';
 import type { IAuctionItem } from 'AuctionItem';
-import { IoPricetagsOutline } from 'react-icons/io5';
-import { LuUsers } from 'react-icons/lu';
 import ProductItem from '../common/item/ProductItem';
 import { useNavigate } from 'react-router-dom';
-import { formatCurrencyWithWon } from '@/utils/formatCurrencyWithWon';
+import MinPrice from '../common/atomic/MinPrice';
+import ParticipantCount from '../common/atomic/ParticipantCount';
 
 const OngoingProduct = ({ product }: { product: IAuctionItem }) => {
   const navigate = useNavigate();
   const handleClick = () => navigate(`/auctions/bid/${product.auctionId}`);
-  const formattedPrice = formatCurrencyWithWon(product.minPrice);
 
   return (
     <ProductItem product={product}>
-      <div className='flex'>
-        <div className='flex gap-2'>
-          <IoPricetagsOutline className='text-gray-500' />
-          <p className='text-sm text-gray-500'>시작가</p>
-        </div>
-        <p className='ml-4 font-semibold'>{formattedPrice}</p>
-      </div>
-      <div className='flex'>
-        <div className='flex gap-2'>
-          <LuUsers className='text-gray-500' />
-          <p className='text-sm text-gray-500'>참여자</p>
-        </div>
-        <p className='ml-4 font-semibold'>{`${product.participantCount}명`}</p>
-      </div>
+      <MinPrice price={product.minPrice} />
+      <ParticipantCount count={product.participantCount} />
       <Button
         color={product.isParticipated ? 'black' : 'white'}
         type='button'
--- src/components/user/PreEnrollMyRegister.tsx
+++ src/components/user/PreEnrollMyRegister.tsx
@@ -1,28 +1,13 @@
-import { FaHeart } from 'react-icons/fa';
 import type { IPreAuctionRegisteredItem } from 'AuctionItem';
-import { IoPricetagsOutline } from 'react-icons/io5';
 import ProductItem from '../common/item/ProductItem';
-import { formatCurrencyWithWon } from '@/utils/formatCurrencyWithWon';
+import LikeCount from '../common/atomic/LikeCount';
+import MinPrice from '../common/atomic/MinPrice';
 
-const PreEnrollMyRegister = ({ product }: { product: IPreAuctionRegisteredItem }) => {
-  const formattedPrice = formatCurrencyWithWon(product.minPrice);
-  
+const PreEnrollMyRegister = ({ product }: { product: IPreAuctionRegisteredItem }) => {  
   return (
     <ProductItem product={product}>
-      <div className='flex'>
-        <div className='flex gap-2'>
-          <IoPricetagsOutline className='text-gray-500' />
-          <p className='text-sm text-gray-500'>시작가</p>
-        </div>
-        <p className='ml-4 font-semibold'>{formattedPrice}</p>
-      </div>
-      <div className='flex'>
-        <div className='flex gap-2'>
-          <FaHeart className='text-gray-500' />
-          <p className='text-sm text-gray-500'>좋아요</p>
-        </div>
-        <p className='ml-4 font-semibold'>{`${product.likeCount}`}</p>
-      </div>
+      <MinPrice price={product.minPrice} />
+      <LikeCount count={product.likeCount} />
     </ProductItem>
   );
 };
