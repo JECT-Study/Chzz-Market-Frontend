c3b749cb79d7fa5b1dc7b4edd8ccfb521b8b58fc
JunSeok <ppu1234@naver.com>
2024-10-18 12:27:53 +0900

    style: 기타 스타일 수정

--- src/components/common/atomic/LikeCount.tsx
+++ src/components/common/atomic/LikeCount.tsx
@@ -7,9 +7,9 @@ const LikeCount = ({ count }: { count: number }) => {
       className="flex items-center text-xs sm:text-body2 text-gray2"
     >
       <img src={PriceIcon} alt="좋아요" />
-      <span className='whitespace-nowrap'>
-        {`좋아요 `}
-        <span className="text-xs text-black sm:text-body2Bold">{count}명</span>
+      <span className='overflow-hidden whitespace-nowrap pt-[2px]'>
+        좋아요
+        <span className="ml-1 text-xs text-black sm:text-body2Bold">{count} 명</span>
       </span>
     </div>
   );
