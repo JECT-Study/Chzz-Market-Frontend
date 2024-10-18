2882c04957b15f046b70784995b681ca21f70e22
JunSeok <ppu1234@naver.com>
2024-10-18 23:19:09 +0900

    feat:  경매 timelabel을 1시간 미만이면 ~분 남음으로 수정하고 초단위는 1분 미만으로 수정.

--- src/components/common/atomic/TimeLabel.tsx
+++ src/components/common/atomic/TimeLabel.tsx
@@ -1,14 +1,27 @@
 import { getTimeColor } from '@/utils/getTimeColor';
 
 const TimeLabel = ({ time }: { time: number }) => {
-  const formattedTime = Math.ceil(time / 3600);
-  const color = getTimeColor(formattedTime);
+  let formattedTime = time / 3600;
+  const color = getTimeColor(Math.ceil(time / 3600));
+
+  let remainingTime;
+  if (formattedTime >= 1) remainingTime = `${Math.ceil(formattedTime)}시간 남음`
+  else {
+    formattedTime *= 60;
+    if (formattedTime >= 1) remainingTime = `${Math.ceil(formattedTime)}분 남음`
+    else {
+      formattedTime *= 60;
+      if (formattedTime >= 1) remainingTime = '1분 미만'
+      else remainingTime = '경매 종료'
+    }
+  }
+
   return (
     <div
       aria-label="시간"
-      className={`absolute bottom-0 w-full pt-1 text-center bg-white opacity-80 ${color} border-b-2`}
+      className={`absolute bottom-0 w-full pt-1 text-center bg-white opacity-80 ${color} border-b-2 text-body1`}
     >
-      {formattedTime === 0 ? <p className='text-red-600'>경매 종료</p> : `${formattedTime}시간 남음`}
+      <p className={`${remainingTime === '경매 종료' && 'text-redNotice'}`}>{remainingTime}</p>
     </div>
   );
 };
