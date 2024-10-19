e6ce744e32b5472dc85441fd390647624f34705c
JunSeok <ppu1234@naver.com>
2024-10-19 20:47:49 +0900

    style; modal 스타일 설정

--- src/components/common/ConfirmModal.tsx
+++ src/components/common/ConfirmModal.tsx
@@ -12,10 +12,10 @@ const ConfirmModal = ({ close, title, description, children }: ConfirmModalProps
   return (
     <div className='absolute inset-0 bg-black/30' onClick={close}>
       <div className='flex items-center justify-center w-full h-full'>
-        <div onClick={(e) => e.stopPropagation()} className='flex flex-col w-2/5 gap-3 px-8 py-6 bg-white rounded-lg sm:text-body1 text-body2'>
+        <div onClick={(e) => e.stopPropagation()} className='flex flex-col w-2/5 gap-3 p-8 bg-white rounded-lg sm:text-body1 text-body2'>
           <h3 className="text-heading3">{title}</h3>
           <p className='text-body2'>{description}</p>
-          <div className="flex justify-between w-full gap-3">
+          <div className="flex justify-between w-full gap-3 mt-2">
             <Button type='button' onClick={close} className='w-full' color='white'>
               취소
             </Button>
