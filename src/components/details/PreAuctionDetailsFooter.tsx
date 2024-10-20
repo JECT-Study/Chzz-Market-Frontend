adfd677596b3539f0874302fa6c5b0f33fbbce66
JunSeok <ppu1234@naver.com>
2024-10-20 10:36:31 +0900

    fix: 경매 전환과 좋아요 버튼에서 하트 아이콘 제거

--- src/components/details/PreAuctionDetailsFooter.tsx
+++ src/components/details/PreAuctionDetailsFooter.tsx
@@ -1,5 +1,3 @@
-import HeartOffIcon from '@/assets/icons/heart_off.svg';
-import HeartOnIcon from '@/assets/icons/like_heart.svg';
 import Button from '../common/Button';
 import Layout from '../layout/Layout';
 import { useConvertAuction, useLikeAuctionItem } from './queries';
@@ -13,23 +11,18 @@ interface PreAuctionDetailsFooterProps {
 const PreAuctionDetailsFooter = ({ likeCount, preAuctionId, isSeller }: PreAuctionDetailsFooterProps) => {
   const { mutate: likeAuctionItem } = useLikeAuctionItem();
   const { mutate: convertToAuction, isPending } = useConvertAuction();
-  const HeartIcon = likeCount ? HeartOnIcon : HeartOffIcon;
 
   return (
-    <Layout.Footer type="double">
-      <div className="flex items-center h-full gap-3 basis-1/3">
-        <img src={HeartIcon} className='size-6' alt='하트 아이콘' />
-        <span className="pt-[2px] text-gray1 text-heading3">{`${likeCount} 명`}</span>
-      </div>
+    <Layout.Footer type="single">
       {isSeller
         ?
-        <Button type='button' disabled={isPending} loading={isPending} onClick={() => convertToAuction(preAuctionId)} className="h-full basis-4/5" color="cheeseYellow">
+        <Button type='button' disabled={isPending} loading={isPending} onClick={() => convertToAuction(preAuctionId)} className="w-full h-full" color="cheeseYellow">
           경매 전환하기
         </Button>
         :
         <Button
           type="button"
-          className="h-full basis-4/5"
+          className="w-full h-full"
           color={likeCount ? 'white' : "cheeseYellow"}
           onClick={() => likeAuctionItem(preAuctionId)}
         >
