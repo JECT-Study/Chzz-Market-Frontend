d68bc055eba092c7a57b10ab55025c059007d61e
aquaman122 <swj981128@gmail.com>
2024-10-22 22:30:16 +0900

    fix: 성공한 경매 컴포넌트 수정

--- src/components/order/OrderWonProduct.tsx
+++ src/components/order/OrderWonProduct.tsx
@@ -2,22 +2,37 @@ import type { IUserAuctionWonItem } from 'AuctionItem';
 import { LuUsers } from 'react-icons/lu';
 import ProductItem from '../common/item/ProductItem';
 import { formatCurrencyWithWon } from '@/utils/formatCurrencyWithWon';
-import MinPrice from '../common/atomic/MinPrice';
+import PriceIcon from '@/assets/icons/price.svg';
 import CreatedAt from '../common/atomic/CreatedAt';
 import { useNavigate } from 'react-router-dom';
+import Button from '../common/Button';
 
 const OrderWonProduct = ({ product }: { product: IUserAuctionWonItem }) => {
   const navigate = useNavigate();
   const formattedWinningPrice = formatCurrencyWithWon(product.winningAmount);
+  const formatted = formatCurrencyWithWon(product.minPrice);
 
+  console.log(product);
 
-  const handleClick = () => {
-    navigate(`/auctions/${product.auctionId}/shipping`);
+  const handleButtonClick = () => {
+    if (product.isOrdered) {
+      navigate(`/auctions/${product.auctionId}/shipping`);
+    } else {
+      navigate(`/payment/success?auctionId=${product.auctionId}`);
+    }
   }
 
   return (
-    <ProductItem product={product} onClick={handleClick}>
-      <MinPrice price={product.minPrice} />
+    <ProductItem product={product}>
+      <div
+        aria-label="시작가"
+        className="flex items-center text-xs sm:text-body2 text-gray2"
+      >
+        <img src={PriceIcon} alt="시작가" />
+        <span className='overflow-hidden whitespace-nowrap pt-[2px]'>
+          <span className="ml-1 text-xs text-black sm:text-body2Bold">{formatted}</span>
+        </span>
+      </div>
       <CreatedAt createAt={product.endDateTime} />
       <div className='flex'>
         <div className='flex gap-2'>
@@ -26,6 +41,9 @@ const OrderWonProduct = ({ product }: { product: IUserAuctionWonItem }) => {
         </div>
         <p className='ml-4 font-semibold'>{formattedWinningPrice}</p>
       </div>
+      <Button type='button' onClick={handleButtonClick} className=''>
+        {product.isOrdered ? '결제하기' : '결제 내역 보기'}
+      </Button>
     </ProductItem>
   );
 };
--- src/components/productList/PreEnrollProduct.tsx
+++ src/components/productList/PreEnrollProduct.tsx
@@ -3,14 +3,20 @@ import type { IPreAuctionItem } from 'AuctionItem';
 import ProductItem from '../common/item/ProductItem';
 import MinPrice from '../common/atomic/MinPrice';
 import LikeCount from '../common/atomic/LikeCount';
+import { useNavigate } from 'react-router-dom';
 
 const PreEnrollProduct = ({ product }: { product: IPreAuctionItem }) => {
-  
+  const navigate = useNavigate();
+  const handleProductClick = () => navigate(`/auctions/pre-auction/${product.productId}`)
+  const handleButtonClick = () => {
+
+  }
+
   return (
-    <ProductItem product={product}>
+    <ProductItem product={product} onClick={handleProductClick}>
       <MinPrice price={product.minPrice} />
       <LikeCount count={product.likeCount} />
-      <Button color={product.isLiked ? 'black' : 'white'} type='button' size='small' className={`${product.isLiked ? '' : ''} w-full h-[33px] rounded-sm`}>
+      <Button onClick={handleButtonClick} color={product.isLiked ? 'black' : 'white'} type='button' size='small' className={`${product.isLiked ? '' : ''} w-full h-[33px] rounded-sm`}>
         {product.isLiked ? '좋아요' : '안좋아요'}
       </Button>
     </ProductItem>
