908c02b9c8e98bad63996291d1e296a57cb0fa91
aquaman122 <swj981128@gmail.com>
2024-10-23 13:24:14 +0900

    feat: 경매 목록에서 좋아요 가능하게 수정

--- src/components/productList/PreEnrollProduct.tsx
+++ src/components/productList/PreEnrollProduct.tsx
@@ -4,20 +4,31 @@ import ProductItem from '../common/item/ProductItem';
 import MinPrice from '../common/atomic/MinPrice';
 import LikeCount from '../common/atomic/LikeCount';
 import { useNavigate } from 'react-router-dom';
+import { useDeletePreAuctionHeart } from '../heart/queries';
+import { useQueryClient } from '@tanstack/react-query';
+import { queryKeys } from '@/constants/queryKeys';
 
 const PreEnrollProduct = ({ product }: { product: IPreAuctionItem }) => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const { mutate: deletePreAuction } = useDeletePreAuctionHeart();
   const handleProductClick = () => navigate(`/auctions/pre-auction/${product.productId}`)
-  const handleButtonClick = () => {
-
-  }
+  const confirmDelete = () => deletePreAuction(product.productId, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [queryKeys.PRE_AUCTION_LIST]});
+    }
+  });
 
   return (
     <ProductItem product={product} onClick={handleProductClick}>
       <MinPrice price={product.minPrice} />
       <LikeCount count={product.likeCount} />
-      <Button onClick={handleButtonClick} color={product.isLiked ? 'black' : 'white'} type='button' size='small' className={`${product.isLiked ? '' : ''} w-full h-[33px] rounded-sm`}>
-        {product.isLiked ? '좋아요' : '안좋아요'}
+      <Button onClick={(event) => 
+      {
+        event.stopPropagation();
+        confirmDelete();
+      }} color={product.isLiked ? 'black' : 'white'} type='button' size='small'>
+        {product.isLiked ? '좋아요 취소' : '좋아요'}
       </Button>
     </ProductItem>
   );
