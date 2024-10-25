4f7556e9d49977b45256c6f4b7cdb7acb996a9c4
JunSeok <ppu1234@naver.com>
2024-10-26 01:06:04 +0900

    style: confirm 스타일 수정

--- src/components/common/Confirm.tsx
+++ src/components/common/Confirm.tsx
@@ -12,8 +12,8 @@ const Confirm = ({ type, children, onCloseModal }: ConfirmProps) => {
   const { title, description } = CONFIRM_MESSAGE[type]
 
   return (
-    <div onClick={(e) => e.stopPropagation()} className='min-w-[17rem] min-h-[13rem] w-[45%] max-w-[23rem] flex items-center flex-col justify-between p-8 bg-white rounded-lg sm:text-body1 text-body2'>
-      <div className="flex flex-col w-full gap-3">
+    <div onClick={(e) => e.stopPropagation()} className='min-w-[17rem] min-h-[13rem] w-[48%] max-w-[23rem] flex items-center gap-3 flex-col justify-between p-8 bg-white rounded-lg sm:text-body1 text-body2'>
+      <div className="flex flex-col w-full gap-2">
         <h3 className="sm:text-heading3 text-body2Bold">{title}</h3>
         <p className='sm:text-body2 text-caption'>{description}</p>
       </div>
