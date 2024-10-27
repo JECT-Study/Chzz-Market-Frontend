f5fbd1abcfe6e5f09a2f9451a6be9cc9c9758888
JunSeok <ppu1234@naver.com>
2024-10-27 10:30:01 +0900

    feat: 시작가와 입찰가 2,000,000원 이하로 제한.

--- src/constants/schema.ts
+++ src/constants/schema.ts
@@ -25,10 +25,17 @@ export const RegisterSchema = z.object({
   category: z.string().min(1, '카테고리를 선택해 주세요.'),
   minPrice: z.string().superRefine((value, ctx) => {
     const num = Number(value.replace(/[^\d]/g, ''));
-    if (Number.isNaN(num) || num < 1000) {
+    if (num < 1000) {
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
         message: '최소 1000원 이상 입력해 주세요.',
+      });
+    }
+
+    if (num > 2_000_000) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: '2,000,000원 이하로 입력해 주세요.',
       });
     }
 
@@ -67,10 +74,17 @@ export const getBidSchema = (minPrice: number, curBidAmount: number) =>
   z.object({
     bidAmount: z.string().superRefine((value, ctx) => {
       const num = Number(value.replace(/[^\d]/g, ''));
-      if (Number.isNaN(num) || num < minPrice) {
+      if (num < minPrice) {
         ctx.addIssue({
           code: z.ZodIssueCode.custom,
           message: `시작가보다 높은 금액을 입력해주세요.`,
+        });
+      }
+
+      if (num > 2_000_000) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: '2,000,000원 이하로 입력해 주세요.',
         });
       }
 
