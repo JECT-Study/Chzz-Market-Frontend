087641bc1c54dc4445aa270307d6b23bfbe19c35
aquaman122 <swj981128@gmail.com>
2024-10-29 11:26:14 +0900

    fix: 회원가입 스타일 수정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -18,21 +18,21 @@ const Login = () => {
     <Layout>
       <Layout.Header title="로그인" handleBack={() => navigate('/')} />
       <Layout.Main>
-        <div className="flex flex-col items-center h-[37.5rem] lg:h-[75rem]">
-          <div className="w-full h-[21rem] lg:h-[42rem] flex flex-col justify-center items-center">
+        <div className="flex flex-col items-center h-[37.5rem] web:h-[75rem]">
+          <div className="w-full h-[21rem] web:h-[42rem] flex flex-col justify-center items-center">
             <img 
               src={ChizzImage} 
               alt="logo" 
-              className="w-[4.75rem] h-[4.3125rem] lg:w-[9.5rem] lg:h-[8.625rem]" />
-            <h2 className="text-center pt-5 text-2xl lg:text-4xl font-semibold">치즈 마켓</h2>
+              className="w-[4.75rem] h-[4.3125rem] web:w-[9.5rem] web:h-[8.625rem]" />
+            <h2 className="text-center pt-5 text-2xl web:text-4xl font-semibold">치즈 마켓</h2>
           </div>
 
           <div className="flex flex-col gap-4 items-center w-full mt-[9.375rem]">
-            <div className="flex justify-center w-[20.6875rem] lg:w-[30rem]">
+            <div className="flex justify-center w-[20.6875rem] web:w-[30rem]">
               <button
                 onClick={handleKakaoLogin}
-                className="flex items-center w-full h-[3.125rem] lg:h-[3.25rem] bg-[#FEEA1C] text-black text-lg rounded-lg cursor-pointer hover:bg-[#F4DC00] focus:ring-2 focus:ring-offset-2 focus:ring-[#F4DC00] focus:outline-none"
-                aria-label="네이버 로그인"
+                className="flex items-center w-full h-[3.125rem] web:h-[3.25rem] bg-[#FEEA1C] text-black text-lg rounded-lg cursor-pointer hover:bg-[#F4DC00] focus:ring-2 focus:ring-offset-2 focus:ring-[#F4DC00] focus:outline-none"
+                aria-label="카카오 로그인"
               >
                 <div className="flex items-center justify-center w-12 h-full pl-3 rounded-l-lg">
                   <RiKakaoTalkFill size={36} />
@@ -40,10 +40,10 @@ const Login = () => {
                 <span className="flex-grow mr-6">카카오 로그인</span>
               </button>
             </div>
-            <div className="flex justify-center w-[20.6875rem] lg:w-[30rem]">
+            <div className="flex justify-center w-[20.6875rem] web:w-[30rem]">
               <button
                 onClick={handleNaverLogin}
-                className="flex items-center w-full h-[3.125rem] lg:h-[3.25rem] bg-[#1ec800] text-white text-lg rounded-lg cursor-pointer hover:bg-[#17b000] focus:ring-2 focus:ring-offset-2 focus:ring-[#17b000] focus:outline-none"
+                className="flex items-center w-full h-[3.125rem] web:h-[3.25rem] bg-[#1ec800] text-white text-lg rounded-lg cursor-pointer hover:bg-[#17b000] focus:ring-2 focus:ring-offset-2 focus:ring-[#17b000] focus:outline-none"
                 aria-label="네이버 로그인"
               >
                 <div className="flex items-center justify-center w-12 h-full pl-3 rounded-l-lg">
