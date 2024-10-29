3f36b6a08525070550f04c3a8705aaf2f9be207a
JunSeok <ppu1234@naver.com>
2024-10-29 12:10:13 +0900

    style: 상세정보 스타일 변경

--- src/components/details/DetailsBasic.tsx
+++ src/components/details/DetailsBasic.tsx
@@ -1,36 +1,30 @@
-import ProfileDefaultImage from '@/assets/icons/profile.svg';
 import { CATEGORIES } from '@/constants/categories';
 import { useNavigate } from 'react-router-dom';
 import Price from '../common/atomic/Price';
 
 interface DetailsBasicProps {
-  profileImg: string, nickname: string, productName: string, category: string, minPrice: number
+  productName: string, category: string, minPrice: number
 }
 
-const DetailsBasic = ({ profileImg, nickname, productName, category, minPrice }: DetailsBasicProps) => {
+const DetailsBasic = ({ productName, category, minPrice }: DetailsBasicProps) => {
   const navigate = useNavigate();
 
   const onClickCategory = () => {
-    navigate(`/product/list?category=${category}`, { state: { category: CATEGORIES[category].value }});
+    navigate(`/product/list?category=${category}`, { state: { category: CATEGORIES[category].value } });
   };
 
   return (
-    <div className='flex flex-col gap-2 sm:gap-4'>
-      <div className='flex items-center gap-2'>
-        <img src={profileImg ?? ProfileDefaultImage} alt="판매자 프로필" className='border rounded-full size-10' />
-        <p className='text-body2'>
-          {nickname}
+    <>
+      <div className='flex flex-col gap-[6px] h-[5.25rem]'>
+        <p className='text-heading2'>
+          {productName}
         </p>
+        <span className='inline-flex'>
+          <button onClick={onClickCategory} className='underline cursor-pointer text-gray2 text-body2'>{CATEGORIES[category].value}</button>
+        </span>
+        <Price title='시작가' price={minPrice} />
       </div>
-      <p className='text-heading2'>
-        {productName}
-      </p>
-      <span className='inline-flex'>
-        <button onClick={onClickCategory} className='underline cursor-pointer shrink-0 grow-0 text-gray2 text-body2'>{CATEGORIES[category].value}</button>
-      </span>
-      <Price title='시작가' price={minPrice} />
-    </div>
-
+    </>
   );
 }
 
--- src/pages/PreAuctionDetails.tsx
+++ src/pages/PreAuctionDetails.tsx
@@ -1,19 +1,20 @@
-import { LoaderFunction, useLoaderData, useNavigate } from 'react-router-dom';
+import ProfileDefaultImage from '@/assets/icons/profile.svg';
 import {
   useDeletePreAuction,
   useGetPreAuctionDetailsWithSuspense
 } from '@/components/details/queries';
+import { LoaderFunction, useLoaderData, useNavigate } from 'react-router-dom';
 
 import BoxEditIcon from '@/assets/icons/in_box_edit_time.svg';
 import BoxLikeIcon from '@/assets/icons/in_box_like.svg';
-import { CarouselItem } from '@/components/ui/carousel';
+import ThreeDotsIcon from '@/assets/icons/three_dots.svg';
 import CustomCarousel from '@/components/common/CustomCarousel';
+import Modal from '@/components/common/Modal';
 import DetailsBasic from '@/components/details/DetailsBasic';
 import DetailsOption from '@/components/details/DetailsOption';
-import Layout from '@/components/layout/Layout';
-import Modal from '@/components/common/Modal';
 import PreAuctionDetailsFooter from '@/components/details/PreAuctionDetailsFooter';
-import ThreeDotsIcon from '@/assets/icons/three_dots.svg';
+import Layout from '@/components/layout/Layout';
+import { CarouselItem } from '@/components/ui/carousel';
 import { getTimeAgo } from '@/utils/getTimeAgo';
 
 const PreAuctionDetails = () => {
@@ -48,41 +49,53 @@ const PreAuctionDetails = () => {
           </Modal>)}
       />
       <Layout.Main>
-        <div className='flex flex-col gap-5'>
-          <CustomCarousel length={images.length} loop>
+        <figure>
+          <CustomCarousel contentStyle='-mx-[20px] -mt-[20px] h-[21.25rem]' length={images.length} loop>
             {images.map((img) => (
               <CarouselItem className='flex items-center justify-center' key={img.imageId}>
                 <img src={img.imageUrl} alt={`${productName}${img.imageId}`} />
               </CarouselItem>
             ))}
           </CustomCarousel>
-          <DetailsBasic profileImg={sellerProfileImageUrl} nickname={sellerNickname} productName={productName} minPrice={minPrice} category={category} />
+          <figcaption>
+            {/* 판매자 정보 */}
+            <div className='flex items-center gap-[13px] h-[3.75rem]'>
+              <img src={sellerProfileImageUrl ?? ProfileDefaultImage} alt="판매자 프로필" className='border rounded-full size-[1.875rem]' />
+              <p className='text-body2'>
+                {sellerNickname}
+              </p>
+            </div>
 
-          <div className='flex items-center justify-between border rounded-lg border-gray3'>
-            <div aria-label="수정 시간" className='flex flex-col items-center w-full gap-1 py-4'>
-              <div className='flex items-center gap-1 text-body2 text-gray2'>
-                <img src={BoxEditIcon} alt="수정 시간" className='size-5' />
-                <span className='pt-[2px]'>수정 시간</span>
-              </div>
-              <p className='text-body1Bold text-gray1'>
-                {updatedTime}
+            {/* 상품 정보 */}
+            <div className='flex flex-col gap-4'>
+              <DetailsBasic productName={productName} minPrice={minPrice} category={category} />
+              <div className='flex items-center justify-between border rounded-lg border-gray3'>
+                <div aria-label="수정 시간" className='flex flex-col items-center w-full gap-1 py-4'>
+                  <div className='flex items-center gap-1 text-body2 text-gray2'>
+                    <img src={BoxEditIcon} alt="수정 시간" className='size-5' />
+                    <span className='pt-[2px]'>수정 시간</span>
+                  </div>
+                  <p className='text-body1Bold text-gray1'>
+                    {updatedTime}
+                  </p>
+                </div>
+                <div aria-label="좋아요"
+                  className="flex flex-col items-center w-full gap-1 py-4">
+                  <div className='flex items-center gap-1 text-body2 text-gray2'>
+                    <img src={BoxLikeIcon} alt='좋아요' className='size-4' />
+                    <span className='pt-[2px]'>좋아요</span>
+                  </div>
+                  <p className='text-body1Bold text-gray1'>
+                    {`${likeCount} 명`}
+                  </p>
+                </div>
+              </div >
+              <p className='overflow-y-auto text-body2 text-gray1'>
+                {description}
               </p>
             </div>
-            <div aria-label="좋아요"
-              className="flex flex-col items-center w-full gap-1 py-4">
-              <div className='flex items-center gap-1 text-body2 text-gray2'>
-                <img src={BoxLikeIcon} alt='좋아요' className='size-4' />
-                <span className='pt-[2px]'>좋아요</span>
-              </div>
-              <p className='text-body1Bold text-gray1'>
-                {`${likeCount} 명`}
-              </p>
-            </div>
-          </div >
-          <p className='overflow-y-auto text-body2 text-gray1'>
-            {description}
-          </p>
-        </div>
+          </figcaption>
+        </figure>
       </Layout.Main>
       <PreAuctionDetailsFooter isLiked={isLiked} preAuctionId={preAuctionId} isSeller={isSeller} />
     </Layout >)
