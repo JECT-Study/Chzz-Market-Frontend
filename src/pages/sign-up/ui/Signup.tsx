df57a91c29bf2ffce383c02ecbef0f6c0a2281d0
aquaman122 <swj981128@gmail.com>
2024-11-03 22:45:43 +0900

    fix: signup 버튼 스타일 수정

--- src/pages/sign-up/ui/Signup.tsx
+++ src/pages/sign-up/ui/Signup.tsx
@@ -114,7 +114,7 @@ export const Signup = () => {
               />
             </div>
             <div>
-              <Button type='button' className='w-[5rem] web:w-[5.97rem] h-[3.13rem] border-gray2' onClick={onNicknameCheck}>중복확인</Button>
+              <Button type='button' className='w-[5rem] web:w-[6.5rem] h-[3.13rem] border-gray2' onClick={onNicknameCheck}>중복확인</Button>
             </div>
           </div>
           {nicknameError && (
