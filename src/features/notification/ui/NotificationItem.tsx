cf35791cfbc7d63aa33898994a79df86b5c6112e
JunSeok <ppu1234@naver.com>
2024-11-23 22:28:45 +0900

    fix: 알림 클릭 수정

--- src/features/notification/ui/NotificationItem.tsx
+++ src/features/notification/ui/NotificationItem.tsx
@@ -1,6 +1,7 @@
 import { getTimeAgo } from '@/shared';
 import DefaultImage from '@/shared/assets/icons/default_image.svg';
 import XButtonIcon from '@/shared/assets/icons/x_button.svg';
+import { MouseEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { NOTIFICATION_CONTENTS, type INotification } from '../config';
 import { useDeleteNotification, useReadNotification } from '../model';
@@ -17,9 +18,15 @@ export const NotificationItem = ({ item }: { item: INotification }) => {
     if (NOTIFICATION_CONTENTS[type]?.link && auctionId) navigate(NOTIFICATION_CONTENTS[type].link!(auctionId))
   };
 
+  const handleDelete = (e: MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    deleteNotification(notificationId)
+
+  }
+
   return (
-    <div className={`p-10 flex justify-between items-start gap-3 ${!isRead && 'bg-notificationBgColor'} `} aria-label={`알림 배경_${notificationId}`}>
-      <figure onClick={handleClick} className={`flex justify-between w-full gap-3 ${auctionId ? 'cursor-pointer' : (isRead) ? '' : 'cursor-pointer'}`} aria-label={`알림_${notificationId}`}>
+    <li onClick={handleClick} className={`p-10 flex justify-between items-start gap-3 ${!isRead && 'bg-notificationBgColor cursor-pointer'} `} aria-label={`알림 배경_${notificationId}`}>
+      <figure className='flex justify-between w-full gap-3' aria-label={`알림_${notificationId}`}>
         <figcaption className='flex flex-col flex-1 justify-between min-h-[6rem]'>
           <h4 className='web:text-body1 text-body2' aria-label={`알림 제목${notificationId}`}>
             {message}
@@ -30,11 +37,11 @@ export const NotificationItem = ({ item }: { item: INotification }) => {
         </figcaption>
         <div className='flex items-start gap-3'>
           <img className='object-cover rounded size-24' src={imageUrl ?? DefaultImage} alt={`알림 이미지_${item.notificationId}`} />
-          <button aria-label={`알림 삭제 버튼_${item.notificationId}`} onClick={() => deleteNotification(notificationId)}>
-            <img className='rounded size-4' src={XButtonIcon} alt='알림 삭제 아이콘' />
+          <button aria-label={`알림 삭제 버튼_${item.notificationId}`} onClick={handleDelete}>
+            <img className='inline rounded size-4' src={XButtonIcon} alt='알림 삭제 아이콘' />
           </button>
         </div>
       </figure>
-    </div>
+    </li>
   );
 };
