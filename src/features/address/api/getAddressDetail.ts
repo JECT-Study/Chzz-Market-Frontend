ddc55bf19f5505011c1552acf46deb34f7cb9c96
aquaman122 <swj981128@gmail.com>
2024-11-27 15:29:03 +0900

    feat: update-api[won]

--- src/features/address/api/getAddressDetail.ts
+++ src/features/address/api/getAddressDetail.ts
@@ -1,6 +1,6 @@
 import { API_END_POINT, httpClient } from "@/shared";
 
 export const getAddressDetail = async (auctionId: string) => {
-  const response = await httpClient.get(`${API_END_POINT.AUCTION}/${auctionId}/winning-bid`);
+  const response = await httpClient.get(`${API_END_POINT.AUCTION}/${auctionId}/won`);
   return response.data;
 };
