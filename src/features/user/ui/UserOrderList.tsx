429f8030ee42475bbd45a60bae4336dabd757f7a
aquaman122 <swj981128@gmail.com>
2024-11-28 17:39:53 +0900

    fix: 회원 탈퇴 버튼 Modal 재수정

--- src/features/user/ui/UserOrderList.tsx
+++ src/features/user/ui/UserOrderList.tsx
@@ -4,27 +4,10 @@ import { useDeleteUsers } from '../model/useDeleteUsers';
 import { useNavigate } from 'react-router-dom';
 import { Button, Confirm, Modal, removeToken } from '@/shared';
 
-const userList = [
-  {
-    id: 1,
-    title: '로그아웃',
-  },
-  {
-    id: 2,
-    title: '회원탈퇴',
-  },
-];
-
 export const UserOrderList = () => {
   const { handleLogout } = useAuth();
   const { deleteUser } = useDeleteUsers();
   const navigate = useNavigate();
-
-  const handleItemClick = (title: string) => {
-    if (title === '로그아웃') {
-      handleLogout();
-    }
-  };
 
   const handleDeleteUser = () => {
     deleteUser(undefined, {
@@ -36,30 +19,21 @@ export const UserOrderList = () => {
   };
 
   return (
-    <Modal>
-      <div className="web:mt-10">
-        {userList.map((item) =>
-          item.title === '회원탈퇴' ? (
-            <Modal.Open name="deleteUser" key={item.id}>
-              <div
-                className="flex justify-between w-full py-3 border-b cursor-pointer border-b-gray3"
-              >
-                <h2 className="text-heading3 web:text-heading2 font-medium">{item.title}</h2>
-                <IoIosArrowForward className="text-2xl" />
-              </div>
-            </Modal.Open>
-          ) : (
-            <div
-              key={item.id}
-              className="flex justify-between w-full py-3 border-b cursor-pointer border-b-gray3"
-              onClick={() => handleItemClick(item.title)}
-            >
-              <h2 className="text-heading3 web:text-heading2 font-medium">{item.title}</h2>
-              <IoIosArrowForward className="text-2xl" />
-            </div>
-          )
-        )}
-
+    <div className="web:mt-10">
+      <div
+        className="flex justify-between w-full py-3 border-b cursor-pointer border-b-gray3"
+        onClick={handleLogout}
+      >
+        <h2 className="text-heading3 web:text-heading2 font-medium">로그아웃</h2>
+        <IoIosArrowForward className="text-2xl" />
+      </div>
+      <Modal>
+        <Modal.Open name="deleteUser">
+          <div className="flex justify-between w-full py-3 border-b cursor-pointer border-b-gray3">
+            <h2 className="text-heading3 web:text-heading2 font-medium">회원탈퇴</h2>
+            <IoIosArrowForward className="text-2xl" />
+          </div>
+        </Modal.Open>
         <Modal.Window name="deleteUser">
           <Confirm 
             type="deleteUser"
@@ -75,7 +49,7 @@ export const UserOrderList = () => {
             </Button>
           </Confirm>
         </Modal.Window>
-      </div>
-    </Modal>
+      </Modal>
+    </div>
   );
 };
