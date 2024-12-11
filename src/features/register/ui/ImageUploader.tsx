cd09520aa2d3f2fafff62d7797cb9f60f11cba0f
JunSeok <ppu1234@naver.com>
2024-12-11 15:35:02 +0900

    style: 이미지 preview 스타일수정

--- src/features/register/ui/ImageUploader.tsx
+++ src/features/register/ui/ImageUploader.tsx
@@ -1,8 +1,8 @@
 import { CarouselItem, CustomCarousel, Input } from "@/shared";
 import { useDragAndDrop, useImageUploader } from '../lib';
 
-import { AddImageButton } from '.';
 import DeleteIcon from '@/shared/assets/icons/delete.svg';
+import { AddImageButton } from '.';
 
 interface ImageUploaderProps {
   images: string[];
@@ -14,13 +14,13 @@ export const ImageUploader = ({ images, setImages }: ImageUploaderProps) => {
   const { fileInputRef, deleteImage, handleImage, handleBoxClick } = useImageUploader(images, setImages);
 
   return (
-    <div className='flex flex-col items-center gap-5 web:flex-row'>
+    <div className='flex flex-col items-center gap-5 web:flex-row web:h-32'>
       <AddImageButton handleBoxClick={handleBoxClick} length={images.length} />
       <CustomCarousel contentStyle='py-3' length={images.length}>
         {images.map((image: string, index: number) => (
           <CarouselItem className='pr-2 basis-1/2 image:basis-1/3' key={image}>
             <div
-              className={`relative transition-transform flex h-32 justify-center items-center duration-400 mx-3 ${index === hoveredIndex ? 'transform scale-105' : ''}`}
+              className={`relative transition-transform flex justify-center h-32 items-center duration-400 mx-3 ${index === hoveredIndex ? 'transform scale-105' : ''}`}
               draggable
               onDragStart={() => handleDragStart(index)}
               onDragOver={(e) => {
@@ -30,13 +30,14 @@ export const ImageUploader = ({ images, setImages }: ImageUploaderProps) => {
               onDragLeave={handleDragLeave}
               onDrop={() => handleDrop(index)}
             >
-              <img src={image} alt={`상품 사진 ${index}`} className='relative w-full h-32' />
+              <img src={image} alt={`상품 사진 ${index}`} className='relative w-full h-full' />
               {index === 0 && (
                 <p className='absolute text-[8px] web:text-xs rounded py-1 px-2 text-white bg-[#454545]/90 top-2 left-1/2 transform -translate-x-1/2'>
                   대표 사진
                 </p>
               )}
               <button
+                type="button"
                 className='absolute top-[-5%] right-[-5%] cursor-pointer text-black size-6'
                 onClick={() => deleteImage(index)}
                 aria-label={`사진 삭제 ${index}`}
