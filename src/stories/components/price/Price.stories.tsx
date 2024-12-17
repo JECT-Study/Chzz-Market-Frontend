4191d76fac06bb9b64ad5a2657af8261e86a797f
aquaman122 <swj981128@gmail.com>
2024-12-17 15:34:45 +0900

    feat: stories 공통 컴포넌트 추가

---
+++ src/stories/components/price/Price.stories.tsx
@@ -0,0 +1,23 @@
+import { Price } from "@/shared";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Price> = {
+  title: 'components/Price',
+  component: Price,
+  tags: ['autodocs'],
+  argTypes: {
+    title: { control: 'text', description: '가격 제목' },
+    price: { control: 'number', description: '가격 값 (숫자)' },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Price>;
+
+export const Default: Story = {
+  args: {
+    title: '판매가',
+    price: 10000,
+  },
+};
