1b49f040d292ede2683d339d69787f4ee7485869
JunSeok <ppu1234@naver.com>
2025-01-07 12:29:41 +0900

    style: 홈 검색 헤더 수정

--- src/pages/home/ui/Home.tsx
+++ src/pages/home/ui/Home.tsx
@@ -3,7 +3,6 @@ import BestItemList from './BestItemList';
 import CategoryList from './CategoryList';
 import HomeItemField from './HomeItemField';
 import { HomeRegisterBtn } from './HomeRegisterBtn';
-import { HomeSearchBar } from './HomeSearchBar';
 import ImminentItemList from './ImminentItemList';
 import PreAuctionItemList from './PreAuctionItemList';
 
@@ -13,7 +12,6 @@ export const Home = () => {
   return (
     <div ref={elementRef} role='main' aria-label='main_area' className='relative flex flex-col justify-between w-full h-full gap-6 overflow-y-scroll'>
       <div className='flex flex-col gap-10'>
-        <HomeSearchBar />
         <HomeItemField name='베스트 경매'>
           <BestItemList />
         </HomeItemField>
