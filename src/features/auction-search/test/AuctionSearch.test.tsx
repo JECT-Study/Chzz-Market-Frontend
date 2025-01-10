1582df6afb5929baceab6d4c9bf944d3976b2236
aquaman122 <swj981128@gmail.com>
2025-01-10 19:45:31 +0900

    fix: 검색 테스트코드 수정

--- src/features/auction-search/test/AuctionSearch.test.tsx
+++ src/features/auction-search/test/AuctionSearch.test.tsx
@@ -18,35 +18,37 @@ const renderComponent = () => {
 }
 
 describe('AuctionSearch', () => {
+  // 모킹된 함수 호출 기록 초기화, 상호 간섭 방지
   beforeEach(() => {
     vi.clearAllMocks();
   });
 
-  test('input tab 렌더링 확인', () => {
+  test('입력창 및 탭이 렌더링 확인', () => {
     renderComponent();
-
+    // 검색 입력창 렌더링 확인
     expect(screen.getByPlaceholderText('검색어를 입력하세요')).toBeInTheDocument();
   });
 
   test('데이터 패치 시 로딩 스피너 확인', async () => {
     (getAuctionSearch as Mock).mockResolvedValueOnce({ items: [] });
+    (getPreAuctionSearch as Mock).mockResolvedValueOnce({ items: [] });
 
     renderComponent();
       
+    // 검색 입력 시 getAuctionSearch API 호출 확인
     const input = screen.getByPlaceholderText('검색어를 입력하세요');
     fireEvent.change(input, { target: { value: 'test'} });
 
     await waitFor(() => expect(getAuctionSearch).toHaveBeenCalledTimes(1));
-
     expect(screen.getByText('검색 결과가 없습니다.')).toBeInTheDocument();
 
+    // 사전 경매 탭 클릭
+    await waitFor(() => screen.getByText(/사전 경매/i));  // 탭이 렌더링될 때까지 기다림
+    const preAuctionTab = screen.getByText(/사전 경매/i);
+    fireEvent.click(preAuctionTab);
 
-    (getPreAuctionSearch as Mock).mockResolvedValueOnce({ items: [] });
-    fireEvent.click(screen.getByText('사전 경매'));
-    
-    // await waitFor(() => expect(getPreAuctionSearch).toHaveBeenCalledTimes(1));
-
-    // expect(screen.getByText('검색 결과가 없습니다.')).toBeInTheDocument();
+    // await waitFor(() => expect(getPreAuctionSearch).toHaveBeenCalledTimes(1));  // 호출이 완료될 때까지 기다림
+    // await waitFor(() => expect(screen.getByText('검색 결과가 없습니다.')).toBeInTheDocument());
   });
 
   test('검색 결과가 없을 때 "검색 결과가 없습니다"'), async () => {
