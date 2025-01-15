481f81b310eccf23d8fd81779effa15055e96481
JunSeok <ppu1234@naver.com>
2025-01-15 17:06:17 +0900

    style: home style 수정

--- src/pages/home/ui/HomeItemField.tsx
+++ src/pages/home/ui/HomeItemField.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 const HomeItemField = ({ children, name }: { children: ReactNode, name: string }) => {
   return (
-    <section className='flex flex-col w-full gap-4'>
+    <section className='flex flex-col w-full gap-6'>
       <label className='text-heading3'>{name}</label>
       <div className='flex gap-4'>
         <LocalAPIAsyncBoundary >
