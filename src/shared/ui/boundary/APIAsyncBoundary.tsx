8fb20b1226b8de065d2bf9900cb068e8bb375a3f
JunSeok <ppu1234@naver.com>
2025-01-15 13:54:10 +0900

    style: boundary fallback style 변경

--- src/shared/ui/boundary/APIAsyncBoundary.tsx
+++ src/shared/ui/boundary/APIAsyncBoundary.tsx
@@ -10,21 +10,30 @@ import { getErrorByCode } from '../../utils/getErrorByCode';
 import { Button } from '../Button';
 import { GlobalSpinner } from '../spinner';
 
-const FallbackComponent = ({ error, resetErrorBoundary }: FallbackProps) => {
+interface FallbackComponentProps extends FallbackProps {
+  header?: string
+}
+
+const FallbackComponent = ({ error, resetErrorBoundary, header }: FallbackComponentProps) => {
   const { title, description } = getErrorByCode(error)
   if (!isAxiosError(error)) throw error
 
   return (
-    <div className='flex flex-col items-center min-w-[10rem] justify-center h-full gap-5'>
-      <img src={ErrorIcon} alt='에러 아이콘' />
-      <div className='space-y-2 text-center'>
-        <h2 className='web:text-heading2 text-heading3 text-gray1'>{title}</h2>
-        <p className='text-gray2 text-body2'>{description}</p>
+    <>
+      {header && <Layout.Header title={header} />}
+      <div className='flex flex-col items-center min-w-[10rem] justify-center h-full gap-5'>
+        <img src={ErrorIcon} alt='에러 아이콘' />
+        <div className='space-y-2 text-center'>
+          <h2 className='web:text-heading2 text-heading3 text-gray1'>{title}</h2>
+          <p className='text-gray2 text-body1'>{description}</p>
+        </div>
       </div>
-      <Button type='button' color='cheeseYellow' className='' onClick={resetErrorBoundary}>
-        다시 불러오기
-      </Button>
-    </div>
+      <Layout.Footer type='single'>
+        <Button type='button' color='cheeseYellow' className='w-full h-full' onClick={resetErrorBoundary}>
+          다시 불러오기
+        </Button>
+      </Layout.Footer>
+    </>
   );
 };
 
@@ -40,7 +49,7 @@ export const APIAsyncBoundary = ({ children, header }: { children: ReactNode; he
 
   return (
     <QueryErrorResetBoundary>
-      {({ reset }) => (<ErrorBoundary onReset={reset} FallbackComponent={FallbackComponent} resetKeys={[pathname, key]}>
+      {({ reset }) => (<ErrorBoundary onReset={reset} FallbackComponent={(props) => <FallbackComponent {...props} header={header} />} resetKeys={[pathname, key]}>
         <Suspense fallback={fallback}>
           {children}
         </Suspense>
