608fcf4fa9d4e250b339f31ccb8523f19c14ef9b
JunSeok <ppu1234@naver.com>
2025-01-17 00:21:07 +0900

    style: boundary 스타일 변경

--- src/pages/home/ui/HomeItemField.tsx
+++ src/pages/home/ui/HomeItemField.tsx
@@ -5,7 +5,7 @@ const HomeItemField = ({ children, name }: { children: ReactNode, name: string }
   return (
     <section className='flex flex-col w-full gap-6'>
       <label className='text-heading3'>{name}</label>
-      <div className='flex gap-4'>
+      <div className='flex gap-4 min-h-[16rem] h-[16rem]'>
         <PartialAsyncBoundary >
           {children}
         </PartialAsyncBoundary>
