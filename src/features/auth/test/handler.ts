bd17dbd31db9b574021b3c8534d75a3c412b8137
JunSeok <ppu1234@naver.com>
2025-02-02 14:01:44 +0900

    fix: mockRefreshToken 수정

--- src/features/auth/test/handler.ts
+++ src/features/auth/test/handler.ts
@@ -2,63 +2,70 @@ import { HttpHandler, HttpResponse, http } from 'msw';
 
 import { API_END_POINT } from '@/shared';
 
-export const kakaoLoginHandler: HttpHandler = http.get(`${import.meta.env.VITE_API_URL}/oauth2/authorization/kakao`, async () => {
-  const mockResponse = {
-    status: 'success',
-    data: {
-      accessToken: 'mockKakaoAccessToken',
-      refreshToken: 'eyJhbGciOiJIUzI1NiJ9.eyJ0eXBlIjoiUkVGUkVTSCIsImlkIjo5LCJyb2xlIjoiVVNFUiIsImlhdCI6MTczNzk2OTY2NywiZXhwIjoxNzM4MDU2MDY3fQ.wNobuJB2VOf_P6i7CyZc1N6OM',
-      nickname: 'mockKakaoUser',
-      bio: 'Hi, I am a mocked Kakao user.',
-    },
-  };
+export const kakaoLoginHandler: HttpHandler = http.get(
+  `${import.meta.env.VITE_API_URL}/oauth2/authorization/kakao`,
+  async () => {
+    const mockResponse = {
+      status: 'success',
+      data: {
+        accessToken: 'mockKakaoAccessToken',
+        refreshToken: 'mockKakaoRefreshToken',
+        nickname: 'mockKakaoUser',
+        bio: 'Hi, I am a mocked Kakao user.'
+      }
+    };
 
-  document.cookie = `REFRESH=${mockResponse.data.refreshToken}; Path=/;`;
+    document.cookie = `REFRESH=${mockResponse.data.refreshToken}; Path=/;`;
+    localStorage.setItem('accessToken', mockResponse.data.accessToken);
 
-  localStorage.setItem('accessToken', mockResponse.data.accessToken);
-
-  return new HttpResponse(JSON.stringify(mockResponse), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-});
-
-export const naverLoginHandler: HttpHandler = http.get(`${import.meta.env.VITE_API_URL}/oauth2/authorization/naver`, async () => {
-  const mockResponse = {
-    status: 'success',
-    data: {
-      accessToken: 'mockKakaoAccessToken',
-      refreshToken: 'eyJhbGciOiJIUzI1NiJ9.eyJ0eXBlIjoiUkVGUkVTSCIsImlkIjo5LCJyb2xlIjoiVVNFUiIsImlhdCI6MTczNzk2OTY2NywiZXhwIjoxNzM4MDU2MDY3fQ.wNobuJB2VOf_P6i7CyZc1N6OM',
-      nickname: 'mockNaverUser',
-      bio: 'Hi, I am a mocked Naver user.',
-    },
-  };
-
-  document.cookie = `REFRESH=${mockResponse.data.refreshToken}; Path=/;`;
-
-  localStorage.setItem('accessToken', mockResponse.data.accessToken);
-
-  return new HttpResponse(JSON.stringify(mockResponse), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-});
-
-export const postSignupHandler: HttpHandler = http.post(`${API_END_POINT.SIGNUP}`, async () => {
-  return new HttpResponse(
-    JSON.stringify({
-      status: 'created',
-      message: 'success',
-    }),
-    {
-      status: 201,
+    return new HttpResponse(JSON.stringify(mockResponse), {
+      status: 200,
       headers: {
-        Authorization: 'Bearer accessToken',
-      },
-    }
-  );
-});
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+);
+
+export const naverLoginHandler: HttpHandler = http.get(
+  `${import.meta.env.VITE_API_URL}/oauth2/authorization/naver`,
+  async () => {
+    const mockResponse = {
+      status: 'success',
+      data: {
+        accessToken: 'mockNaverAccessToken',
+        refreshToken: 'mockNaverRefreshToken',
+        nickname: 'mockNaverUser',
+        bio: 'Hi, I am a mocked Naver user.'
+      }
+    };
+
+    document.cookie = `REFRESH=${mockResponse.data.refreshToken}; Path=/;`;
+    localStorage.setItem('accessToken', mockResponse.data.accessToken);
+
+    return new HttpResponse(JSON.stringify(mockResponse), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+);
+
+export const postSignupHandler: HttpHandler = http.post(
+  `${API_END_POINT.SIGNUP}`,
+  async () => {
+    return new HttpResponse(
+      JSON.stringify({
+        status: 'created',
+        message: 'success'
+      }),
+      {
+        status: 201,
+        headers: {
+          Authorization: 'Bearer accessToken'
+        }
+      }
+    );
+  }
+);
