dd7d00b19e53f3c3fc3005662037f302e10e88de
JunSeok <ppu1234@naver.com>
2025-02-14 18:56:04 +0900

    fix: 경로 수정

--- src/features/auth/test/handler.ts
+++ src/features/auth/test/handler.ts
@@ -1,6 +1,6 @@
 import { HttpHandler, HttpResponse, http } from 'msw';
 
-import { API_END_POINT } from '@/shared';
+import { API_END_POINT } from '@/shared/constants/apiEndPoint';
 
 export const kakaoLoginHandler: HttpHandler = http.get(
   `${import.meta.env.VITE_API_URL}/oauth2/authorization/kakao`,
