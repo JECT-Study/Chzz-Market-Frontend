e50614bac7970e64dcd2c266fef2e42cec47f365
JunSeok <ppu1234@naver.com>
2025-02-21 18:36:21 +0900

    fix login mocking 삭제

--- src/features/auth/hooks/useAuth.ts
+++ src/features/auth/hooks/useAuth.ts
@@ -1,48 +1,17 @@
-import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
-import { useNavigate } from 'react-router';
 import { removeToken } from '@/shared/utils/token';
+import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router';
 import { logout } from '../api/logout';
 
 export const useAuth = () => {
   const navigate = useNavigate();
 
   const handleKakaoLogin = async () => {
-    if (import.meta.env.VITE_USE_MOCK === 'true') {
-      // Mocking 환경
-      const response = await axios.get(
-        `${import.meta.env.VITE_API_URL}/oauth2/authorization/kakao`
-      );
-
-      const { data } = response;
-
-      // Mocked 응답 처리
-      localStorage.setItem('accessToken', data.data.accessToken);
-      document.cookie = `REFRESH=${data.data.refreshToken}; Path=/;`;
-
-      window.location.href = '/login?status=success';
-    } else {
-      window.location.href = `${import.meta.env.VITE_API_URL}/oauth2/authorization/kakao`;
-    }
+    window.location.href = `${import.meta.env.VITE_API_URL}/oauth2/authorization/kakao`;
   };
 
   const handleNaverLogin = async () => {
-    if (import.meta.env.VITE_USE_MOCK === 'true') {
-      // Mocking 환경
-      const response = await axios.get(
-        `${import.meta.env.VITE_API_URL}/oauth2/authorization/kakao`
-      );
-
-      const { data } = response;
-
-      // Mocked 응답 처리
-      localStorage.setItem('accessToken', data.data.accessToken);
-      document.cookie = `REFRESH=${data.data.refreshToken}; Path=/;`;
-
-      window.location.href = '/login?status=success';
-    } else {
-      window.location.href = `${import.meta.env.VITE_API_URL}/oauth2/authorization/naver`;
-    }
+    window.location.href = `${import.meta.env.VITE_API_URL}/oauth2/authorization/naver`;
   };
 
   const logoutMutation = useMutation({
