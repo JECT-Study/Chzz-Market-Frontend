d9f92b193afb17b3487732954261d4be60a258f1
aquaman122 <swj981128@gmail.com>
2025-02-25 10:57:11 +0900

    refactoring: 로그인 코드 이전 코드로 롤백

--- src/features/auth/hooks/useAuth.ts
+++ src/features/auth/hooks/useAuth.ts
@@ -2,16 +2,43 @@ import { removeToken } from '@/shared';
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router';
 import { logout } from '..';
+import axios from 'axios';
 
 export const useAuth = () => {
   const navigate = useNavigate();
 
-  const handleKakaoLogin = () => {
-    window.location.href = `${import.meta.env.VITE_API_URL}/oauth2/authorization/kakao`;
+  const handleKakaoLogin = async () => {
+    if (import.meta.env.VITE_USE_MOCK === 'true') {
+      // Mocking 환경
+      const response = await axios.get(`${import.meta.env.VITE_API_URL}/oauth2/authorization/kakao`);
+
+      const { data } = response;
+
+      // Mocked 응답 처리
+      localStorage.setItem('accessToken', data.data.accessToken);
+      document.cookie = `REFRESH=${data.data.refreshToken}; Path=/;`;
+
+      window.location.href = '/login?status=success';
+    } else {
+      window.location.href = `${import.meta.env.VITE_API_URL}/oauth2/authorization/kakao`;
+    }
   };
 
-  const handleNaverLogin = () => {
-    window.location.href = `${import.meta.env.VITE_API_URL}/oauth2/authorization/naver`;
+  const handleNaverLogin = async () => {
+    if (import.meta.env.VITE_USE_MOCK === 'true') {
+      // Mocking 환경
+      const response = await axios.get(`${import.meta.env.VITE_API_URL}/oauth2/authorization/naver`);
+
+      const { data } = response;
+
+      // Mocked 응답 처리
+      localStorage.setItem('accessToken', data.data.accessToken);
+      document.cookie = `REFRESH=${data.data.refreshToken}; Path=/;`;
+
+      window.location.href = '/login?status=success';
+    } else {
+      window.location.href = `${import.meta.env.VITE_API_URL}/oauth2/authorization/naver`;
+    }
   };
 
   const logoutMutation = useMutation({
